ccf040f2e31d556a7046d0f7572445d9dc70b2c6
noobchirag69 <chiragchakraborty48@gmail.com>
2022-06-23 11:07:04 +0530

    Switched from Ajax to fetch API

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
 
 let numberInput = document.querySelector('#numberInput');
-numberInput.addEventListener('input', getFactAjax);
+numberInput.addEventListener('input', getFactFetch);
 
 // Fetch with XHR
 function getFactAjax() {
